63244f7db4d6a592e9c5af905b9c1991ae81803c
Jonathan Bramble <j.p.bramble@leeds.ac.uk>
2012-01-12 10:11:52 +0000

    added boost macro

--- config.h
+++ config.h
@@ -1,6 +1,9 @@
 /* config.h.  Generated from config.h.in by configure.  */
 /* config.h.in.  Generated from configure.ac by autoheader.  */
 
+/* define if the Boost library is available */
+#define HAVE_BOOST /**/
+
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT "jbramble82@hotmail.com"
 
@@ -14,7 +17,7 @@
 #define PACKAGE_TARNAME "Moduli"
 
 /* Define to the home page for this package. */
-#define PACKAGE_URL "http://www.leeds.ac.uk"
+#define PACKAGE_URL "http://mnp.leeds.ac.uk"
 
 /* Define to the version of this package. */
 #define PACKAGE_VERSION "0.1"
